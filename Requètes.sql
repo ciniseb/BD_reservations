451940ec8c7c51a54b891b3eeb926fb660ac655e
Boie0601 <Boie0601@usherbrooke.ca>
2023-05-23 15:23:36 -0400

    hehe

--- Requètes.sql
+++ Requètes.sql
@@ -25,10 +25,27 @@ CREATE OR REPLACE FUNCTION Tableau6(debut timestamp, fin timestamp, Categorie In
     CIP CHAR(8),
     intervalle INTERVAL
     )
-
 As $$
+    DECLARE Categorie2 INT;
 BEGIN
+
+    if(Categorie = 0121)
+    Then
+        Categorie2 = 0111;
+    END IF ;
+
+
+
+    if(Categorie = 0111 or Categorie = 0110 or Categorie = 012 or Categorie = 0210 or Categorie = 0211
+        or Categorie = 0212 or Categorie = 0213 or Categorie = 0214 or Categorie = 0215 or Categorie = 0216
+           or Categorie = 0217 or Categorie = 0372 or Categorie = 0373 or Categorie = 0510 or Categorie = 0620
+           or Categorie = 0625 or Categorie = 0640 or Categorie = 0930 or Categorie = 1030 or Categorie = 1260)
+    Then
+        Categorie2 = 0121;
+    END IF ;
+
     RETURN QUERY
+
         SELECT  generated_time, réservation.id_pavillon, réservation.id_local, réservation.cip, réservation.intervalle
         FROM bd_reservations.local, bd_reservations.réservation
                         Left JOIN (
@@ -39,11 +56,17 @@ BEGIN
                        ) AS generated_time
         ) AS time_series ON réservation.date <= generated_time AND generated_time <= réservation.date + réservation.intervalle
         where réservation.id_local = local.id_local and local.id_catégorie = Categorie and réservation.date <= generated_time AND generated_time <= réservation.date + réservation.intervalle
+        or réservation.id_local = local.id_local and local.id_catégorie = Categorie2 and réservation.date <= generated_time AND generated_time <= réservation.date + réservation.intervalle
         ORDER BY generated_time;
 END
 $$ language plpgsql;
 
 select * from Tableau6('2023-12-12 08:30:00', '2023-12-12 22:30:00', 0110);
 
+select * from Tableau6('2023-12-12 08:30:00', '2023-12-12 22:30:00', 0111);
+
+select * from Tableau6('2023-12-12 08:30:00', '2023-12-12 22:30:00', 0121);
+
 select * from Tableau6('2023-12-13 08:30:00', '2023-12-13 22:30:00', 0110);
+
 
